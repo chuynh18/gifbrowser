b4f574da230f4d42b78c467fee8ee8ad2d261389
Acer <38088640+chuynh18@users.noreply.github.com>
2018-05-03 19:51:49 -0700

    ensure accurate instructional message is always displayed, even if user removes all the buttons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,61 +4,77 @@ var removeMode = false;
 
 $(function() {
 
-var topics = ["Cool 3D World", "sfm", "prequel memes", "computational fluid dynamics", "monte carlo methods", "finite element analysis", "bad cgi"];
+    // this stores the default gif searches
+    var topics = ["Cool 3D World", "sfm", "prequel memes", "computational fluid dynamics", "finite element analysis", "bad cgi"];
 
-for (var i = 0; i < topics.length; i++) {
-    var gifButton = $("<div>");
-    gifButton.addClass("searchGif");
-    gifButton.attr("value", topics[i]);
-    gifButton.text(topics[i]);
-    $("#button").append(gifButton);
-}
-
-$("#buttonAdd").on("click", function(event) {
-    var buttons = $("<div>");
-    var enteredText = $("#gifInput").val();
-    if (enteredText === "") {
-        alert("Enter some text first!");
+    // this appends the default gif searches
+    for (var i = 0; i < topics.length; i++) {
+        var gifButton = $("<div>");
+        gifButton.addClass("searchGif");
+        gifButton.attr("value", topics[i]);
+        gifButton.text(topics[i]);
+        $("#button").append(gifButton);
     }
-    else {
-        buttons.text(enteredText);
-        buttons.attr("value", enteredText);
-        buttons.addClass("searchGif");
-        $("#button").append(buttons);
-        $("#gifInput").val("");
-        event.preventDefault();
-    }
-  });
 
-});
+    $("#buttonAdd").on("click", function(event) {
+        var buttons = $("<div>");
+        var enteredText = $("#gifInput").val();
+        if (enteredText === "") {
+            alert("Enter some text first!");
+        }
+        else {
+            buttons.text(enteredText);
+            buttons.attr("value", enteredText);
+            buttons.addClass("searchGif");
+            $("#button").append(buttons);
+            $("#gifInput").val("");
+            event.preventDefault();
+        };
+    });
 
-// replace with api call
-var searchGifOnClick = function () {
-    console.log("hey, you clicked " + $(this).attr("value"));
-};
-
-// the thing that actually listens for the click
-$(document).on("click", ".searchGif", searchGifOnClick);
-
-var deleteModeOn = function() {
-    $(this).remove();
-};
-
-$(document).on("click", "#deleteMode", function() {
-    if (removeMode === false) {
-        removeMode = true;
-        $(document).off("click", ".searchGif", searchGifOnClick);
-        console.log("remove mode set to: " + removeMode);
-        $("#deleteModeMessage").html("<font color='red'><b>Delete mode enabled.</b>  You can remove GIF search buttons by clicking on them.</font>")
-        $("#deleteMode").css("background-color", "red");
-        $(document).on("click", ".searchGif", deleteModeOn);
-    }
-    else if (removeMode === true) {
-        removeMode = false;
-        $("#deleteMode").css("background-color", "lightcyan");
-        console.log("remove mode set to: " + removeMode);
-        $("#deleteModeMessage").text("Click a button above to retrieve GIFs!")
-        $(document).on("click", ".searchGif", searchGifOnClick);
-        $(document).off("click", ".searchGif", deleteModeOn);
+    // replace with api call
+    var searchGifOnClick = function () {
+        console.log("hey, you clicked " + $(this).attr("value"));
     };
+
+    // the thing that actually listens for the click
+    $(document).on("click", ".searchGif", searchGifOnClick);
+
+    // this is the function that removes GIF search buttons
+    var deleteModeOn = function() {
+        $(this).remove();
+    };
+
+    // this function toggles between making the gif buttons remove themselves or searching for gifs
+    // it works by creating the proper on click event handler while deleting the other one
+    $(document).on("click", "#deleteMode", function() {
+        if (removeMode === false) {
+            removeMode = true;
+            $(document).off("click", ".searchGif", searchGifOnClick);
+            console.log("remove mode set to: " + removeMode);
+            $("#deleteMode").css("background-color", "red");
+            $(document).on("click", ".searchGif", deleteModeOn);
+        }
+        else if (removeMode === true) {
+            removeMode = false;
+            $("#deleteMode").css("background-color", "lightcyan");
+            console.log("remove mode set to: " + removeMode);
+            $(document).on("click", ".searchGif", searchGifOnClick);
+            $(document).off("click", ".searchGif", deleteModeOn);
+        };
+    });
+
+    // this function ensures the appropriate instructional message is always displayed
+    $(document).on("click", function() {
+        if ($("#button").children().length === 0) {
+            $("#deleteModeMessage").html("<font color='red'><b>You've removed all the buttons.</b>  Add buttons to search for GIFs!</font>")
+        }
+        else if (removeMode === true) {
+            $("#deleteModeMessage").html("<font color='red'><b>Delete mode enabled.</b>  You can remove GIF search buttons by clicking on them.</font>")
+        }
+        else if (removeMode === false) {
+            $("#deleteModeMessage").text("Click a button above to retrieve GIFs!")
+        };
+    });
+
 });
